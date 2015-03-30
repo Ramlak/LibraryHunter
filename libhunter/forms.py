38ec706cad9c8015d5cee3d2e48a76d5a41d6346
Marcin Kalinowski <kalmar2718@gmail.com>
2015-03-30 18:27:03 +0200

    Fix url form

--- libhunter/forms.py
+++ libhunter/forms.py
@@ -6,7 +6,7 @@ from libhunter.models import LibraryType
 
 class UploadForm(forms.Form):
     file = forms.FileField(label="", widget=forms.FileInput(attrs={'class': 'form-control'}), required=False)
-    url = forms.URLField(label="", widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    url = forms.URLField(label="OR", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'URL to file'}), required=False)
     library_type = forms.ChoiceField(label="", widget=forms.Select(attrs={'class': 'form-control'}), choices=[(lib.id, lib.name) for lib in LibraryType.objects.all()], required=True)
 
 
